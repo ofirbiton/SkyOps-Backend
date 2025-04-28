d44167f3688c32354131e5816a8b9c1ffb1aef2b
Ofir Biton <143535334+ofirbiton@users.noreply.github.com>
2025-04-28 20:04:57 +0300

    Delete utils directory

--- utils/file_dialog.py
+++
@@ -1,7 +0,0 @@
-from shared import dependencies as dep
-
-
-def select_image_file():
-    root = dep.tk.Tk()
-    root.withdraw()
-    return dep.filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp")])
