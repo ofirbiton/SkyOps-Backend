0feadde15a05595a4ec0918ca9da84333c418bf9
Ofir Biton <143535334+ofirbiton@users.noreply.github.com>
2025-05-20 00:12:02 +0300

    Update dependencies.py

--- shared/dependencies.py
+++ shared/dependencies.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import tkinter as tk
-from tkinter import filedialog
+#import tkinter as tk
+#from tkinter import filedialog
 from skimage.morphology import skeletonize
 from skimage.measure import label, regionprops
 from skimage.draw import line
