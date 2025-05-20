a69c207344d9eb6f59b45dc522ccc8206833892d
Noe-Mignolet <146870718+Noe-Mignolet@users.noreply.github.com>
2025-05-20 03:53:16 +0300

    Delete routes.py

--- routes.py
+++
@@ -1,7 +0,0 @@
-from flask import request
-from services import mission_service
-
-def register_routes(app):
-    @app.route("/api/create-mission", methods=["POST"])
-    def create_mission_route():
-        return mission_service.create_mission(request)
